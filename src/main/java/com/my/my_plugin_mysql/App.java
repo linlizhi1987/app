03c29cafd5047d4fa463a12c31f6b24dcfd14eed
linlizhi1987 <linlizhi1987@163.com>
2016-11-15 15:53:17 +0800

    first app create

---
+++ src/main/java/com/my/my_plugin_mysql/App.java
@@ -0,0 +1,16 @@
+package com.my.my_plugin_mysql;
+
+import com.google.gson.Gson;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	
+        System.out.println( "Hello World!" );
+    }
+}
